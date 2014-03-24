7504ab8db8b352a25e2eaac05b1ab56b0ed91a7e
megazear7 <alexlockhart7@gmail.com>
2014-03-24 16:13:49 -0400

    nested building attributes now working on schools

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ActiveRecord::Base
-  has_many :buildings
+  has_many :buildings, :dependent => :destroy
+  accepts_nested_attributes_for :buildings#, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
