3f1f1b601b84e4caab9e106ba4a16015a68873b3
megazear7 <alexlockhart7@gmail.com>
2014-03-31 23:00:25 -0400

    trying to give users the ability to select a school

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,5 @@
 class School < ActiveRecord::Base
   has_many :buildings, :dependent => :destroy
   accepts_nested_attributes_for :buildings#, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :study_sessions
   has_many :notes
+  belongs_to :school
 end
