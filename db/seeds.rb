15749e6981c2faf5d14604dbea32af5423afe5f2
megazear7 <alexlockhart7@gmail.com>
2014-04-07 15:41:25 -0400

    made new seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,49 @@
 User.create!([
-  {email: "alexlockhart7@gmail.com", encrypted_password: "$2a$10$36BPf95lSMy9pXJ5jbgSOeg2WK6ONA7pqtJ3DKrhMj7ZwUbg8nQqK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-03-25 02:00:09", last_sign_in_at: "2014-03-25 02:00:09", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+  {email: "alexlockhart7@gmail.com", encrypted_password: "$2a$10$36BPf95lSMy9pXJ5jbgSOeg2WK6ONA7pqtJ3DKrhMj7ZwUbg8nQqK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 13, current_sign_in_at: "2014-04-07 18:05:50", last_sign_in_at: "2014-04-03 01:31:29", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", school_id: 1},
+  {email: "ghostalex7@hotmail.com", encrypted_password: "$2a$10$mhI9UR6eA1i3/QTOpSANtuadTw0fnlbhATi6pUdXSjOaqDIh5buJ.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-04-01 02:13:48", last_sign_in_at: "2014-04-01 02:13:48", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", school_id: 1},
+  {email: "test@test.com", encrypted_password: "$2a$10$1gm/PDqjD9zCMfHN4urtme8jJI/DM828PDVqtDPWR8Sd04yWAYPcK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2014-04-03 00:52:32", last_sign_in_at: "2014-04-02 23:19:53", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", school_id: 4}
 ])
 Building.create!([
   {school_id: 1, name: "Dreese"},
   {school_id: 1, name: "Caldwell"},
-  {school_id: 1, name: "Hitchcock"}
+  {school_id: 1, name: "Hitchcock"},
+  {school_id: 4, name: "Founders"},
+  {school_id: 4, name: "Co Hall"}
 ])
 Note.create!([
-  {study_session_id: 1, summary: "We should make sure to go over the stuff about graphs and stuff."},
-  {study_session_id: 2, summary: "The beginging of chapter 3 in the book helps a lot for this."},
-  {study_session_id: 3, summary: "I can help you with problem 4, but 1-3 I cannot seem to figure out."}
+  {study_session_id: 1, summary: "We should make sure to go over the stuff about graphs and stuff.", user_id: nil},
+  {study_session_id: 2, summary: "The beginging of chapter 3 in the book helps a lot for this.", user_id: nil},
+  {study_session_id: 3, summary: "I can help you with problem 4, but 1-3 I cannot seem to figure out.", user_id: nil},
+  {study_session_id: 5, summary: "Blah Blah Blah", user_id: 1},
+  {study_session_id: 5, summary: "Blah Blah Blah ....", user_id: 1},
+  {study_session_id: 5, summary: "Blah Blah Blah..... Again .... asdfadas", user_id: 1},
+  {study_session_id: 5, summary: "Blah Blah .asdfjahsdjf adfasddddddd", user_id: 2},
+  {study_session_id: 10, summary: "testing", user_id: 1},
+  {study_session_id: 8, summary: "testing note", user_id: 1}
 ])
 Room.create!([
   {building_id: 1, room_number: "172"},
   {building_id: 1, room_number: "254"},
-  {building_id: 1, room_number: "309"},
+  {building_id: 1, room_number: "399"},
   {building_id: 3, room_number: "102"},
   {building_id: 3, room_number: "170"},
   {building_id: 3, room_number: "113"},
-  {building_id: 2, room_number: "101"},
+  {building_id: 2, room_number: "999"},
   {building_id: 2, room_number: "105"},
-  {building_id: 2, room_number: "276"}
+  {building_id: 2, room_number: "276"},
+  {building_id: 7, room_number: "243"},
+  {building_id: 7, room_number: "122"},
+  {building_id: 7, room_number: "423"},
+  {building_id: 1, room_number: "111"},
+  {building_id: 1, room_number: "111"},
+  {building_id: 2, room_number: "888"}
 ])
 School.create!([
-  {name: "Ohio State University"}
+  {name: "Ohio State University"},
+  {name: "COTC"}
 ])
 StudySession.create!([
-  {room_id: 2, name: "Studing for foundatons final", summary: "We are going to go over all the material needed for the foundations final.", date: nil, time: nil},
-  {room_id: 6, name: "Math 1130 midterm", summary: "Lets go over integration.", date: nil, time: nil},
-  {room_id: 9, name: "ECE 2000 Clymer Homework 5", summary: "Homework 5 is impossible!! We need to go over this.", date: nil, time: nil}
+  {room_id: 3, name: "Testing", summary: "Summarry .... .... ... blah blah bah", date: nil, time: nil, user_id: 1, meeting_time: "2013-04-09 07:09:00"},
+  {room_id: 7, name: "Study For Systems Final", summary: "We will be studying for the systems final. Going over assembly.", date: nil, time: nil, user_id: 1, meeting_time: "2014-04-12 13:15:00"},
+  {room_id: 2, name: "Example", summary: "test", date: nil, time: nil, user_id: 1, meeting_time: "2014-04-08 18:46:00"}
 ])
