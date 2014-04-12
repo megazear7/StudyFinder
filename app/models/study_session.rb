d7e442fe10f9ec76786f5817cd8525ca9b3a9d42
lx051068 <rzrz_2003@Yahoo.com.sg>
2014-04-12 02:05:19 -0400

    1: modified study_session controller. in the study_session_params function, added subject. 2: modified study_session model. added subject in the search function. 3: modified view/rooms/show.html.erb, added Subject in the view. 4: modified view/study_sessions/edit.html.erb, added Subject in the view. 5: modified view/study_sessions/index.html.erb, added Subject in the view. 6: modified view/study_sessions/new.html.erb, added Subject in the view. 7: modified view/study_sessions/show.html.erb, added Subject in the view.

--- app/models/study_session.rb
+++ app/models/study_session.rb
@@ -7,10 +7,11 @@ class StudySession < ActiveRecord::Base
     joins([:room => [:building => :school]]).where("
         rooms.room_number LIKE ? OR 
         LOWER(study_sessions.name) LIKE ? OR
+	LOWER(study_sessions.subject) LIKE ? OR
         LOWER(study_sessions.summary) LIKE ? OR
         LOWER(buildings.name) LIKE ? OR
         LOWER(schools.name) LIKE ?
-        ", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
+        ", "%#{search.downcase}%", "%#{search.downcase}%","%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 
 end
