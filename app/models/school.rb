9b5b47beae6f7a8e927746def7b99816c16e510b
lx051068 <rzrz_2003@Yahoo.com.sg>
2014-04-12 23:10:59 -0400

    Modified school model: added validation, no empty name for school.

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,6 +5,8 @@ class School < ActiveRecord::Base
   has_many :study_sessions, through: :buildings
   has_many :users
 
+  validates :name, presence: true
+
   def self.search search
     where("
         LOWER(name) LIKE ?
