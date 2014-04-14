b7b7b1b74a9d443e8d591fd20e63e2be0a99368c
megazear7 <alexlockhart7@gmail.com>
2014-04-14 12:49:48 -0400

    added link from subject

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411183020) do
+ActiveRecord::Schema.define(version: 20140411211530) do
 
   create_table "buildings", force: true do |t|
     t.integer  "school_id"
