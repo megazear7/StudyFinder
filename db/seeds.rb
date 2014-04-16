c28d73f40e26c2174bfad9514bc31cda7874fc78
megazear7 <alexlockhart7@gmail.com>
2014-04-16 10:27:22 -0400

    modified seed file to not break with the whole user interactions thing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-User.create!([
-  {email: "alexlockhart7@gmail.com", encrypted_password: "$2a$10$W.dNSPWcai0PV6Z3PLs38OJV60GUj.i6qlwVw0VSSe5XoCrMUikza", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-04-15 13:16:27", last_sign_in_at: "2014-04-15 13:16:27", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", school_id: 1}
-])
 Building.create!([
   {school_id: 1, name: "Dreese"},
   {school_id: 1, name: "Caldwell"},
@@ -14,9 +11,6 @@ Building.create!([
   {school_id: 1, name: "Neil Building"},
   {school_id: 1, name: "Mathematics Building"},
   {school_id: 1, name: "Macquig Lab"}
-])
-Note.create!([
-  {study_session_id: 1, summary: "Okay, I will find my notes on this.", user_id: 1, image_file_name: "overland.jpg", image_content_type: "image/jpeg", image_file_size: 842130, image_updated_at: "2014-04-15 13:22:40"}
 ])
 Room.create!([
   {building_id: 1, room_number: "171"},
@@ -36,7 +30,4 @@ School.create!([
   {name: "Ohio State University"},
   {name: "COTC"},
   {name: "Test School"}
-])
-StudySession.create!([
-  {room_id: 1, name: "Systems 1 Final", summary: "Lets review C, I forget a lot of the first half of the class.", date: nil, time: nil, user_id: 1, meeting_time: "2014-04-16 13:21:00", subject: "Computer Science"}
 ])
